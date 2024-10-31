a08ad00228ac98334ddbb406dc82566aeb6fd4bf
Aldebaraan <aldecanedo97@gmail.com>
2024-10-31 17:28:46 -0400

    Code for UI added

--- src/main/java/com/example/loancalculator/HelloApplication.java
+++ src/main/java/com/example/loancalculator/HelloApplication.java
@@ -2,22 +2,41 @@ package com.example.loancalculator;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+    private TextField tfAnnualInterestRate = new TextField();
+    private TextField tfNumberOfYears = new TextField();
+    private TextField tfLoanAmount = new TextField();
+    private TextField tfMonthlyPayment = new TextField();
+    private TextField tfTotalPayment = new TextField();
+    private Button btCalculate = new Button("Calculate");
 
-    public static void main(String[] args) {
-        launch();
+    @Override // Override the start method in the Application class
+    public void start(Stage stage) {
+        // Create UI
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(5);
+        gridPane.setVgap(5);
+        gridPane.add(new Label("Annual Interest Rate: "), 0, 0);
+        gridPane.add(tfAnnualInterestRate, 1, 0);
+        gridPane.add(new Label("Number of Years:"), 0, 1);
+        gridPane.add(tfNumberOfYears, 1, 1);
+        gridPane.add(new Label("Loan Amount:"), 0, 2);
+        gridPane.add(tfLoanAmount, 1, 2);
+        gridPane.add(new Label("Monthly Payment:"), 0, 3);
+        gridPane.add(tfMonthlyPayment, 1, 3);
+        gridPane.add(new Label("Total Payment:"), 0, 4);
+        gridPane.add(tfTotalPayment, 1, 4);
+        gridPane.add(btCalculate, 1, 5);
     }
 }
